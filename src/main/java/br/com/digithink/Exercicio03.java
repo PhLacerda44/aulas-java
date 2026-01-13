b43ef91aabae00e4eb1ec40a8824b7d3ad78d542
Pedro Lacerda <pedrolacerda598@gmail.com>
2026-01-13 14:40:30 -0300

    .

---
+++ src/main/java/br/com/digithink/Exercicio03.java
@@ -0,0 +1,37 @@
+package br.com.digithink;
+
+/*
+    Peça dois números inteiros.
+
+    Se forem iguais, faça a soma, caso contrário faça a multiplicação.
+*/
+
+import java.util.Scanner;
+
+public class Exercicio03 {
+
+    void main() {
+
+        // Scanner
+        Scanner obj = new Scanner(System.in);
+
+        // Obter o primeiro número
+        System.out.println("Informe o primeiro número");
+        int numero1 = obj.nextInt();
+
+        // Obter o segundo número
+        System.out.println("Informe o segundo número");
+        int numero2 = obj.nextInt();
+
+        // Fecha o Scanner e libera os recursos associados ao stream de entrada (System.in)
+        obj.close();
+
+        // Operador ternário
+        int calculo = numero1 == numero2 ? numero1+numero2 : numero1*numero2;
+
+        // Exibir o cálculo
+        System.out.println(calculo);
+        
+    }
+    
+}
